0ee2258b264cbca724cd9b647911b3e38d444c03
Hélder Duarte <cossou@gmail.com>
2014-02-13 16:10:12 +0000

    Fixed tests

---
+++ tests/TrakioTest.php
@@ -0,0 +1,53 @@
+<?php
+
+use Cossou\Trakio;
+
+class TrakioTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @param $token
+     *
+     * @dataProvider provideConfigValues
+     */
+    public function testFactoryReturnsClient($token)
+    {
+        $config = array(
+            'token' => $token,
+        );
+
+        $client = Trakio::factory($config);
+
+        $defaultOption = $client->getDefaultOption('headers/X-Token');
+
+        $this->assertInstanceOf('\Cossou\Trakio', $client);
+        $this->assertEquals($config['token'], $defaultOption['token']);
+    }
+
+    /**
+     * @expectedException \Guzzle\Common\Exception\InvalidArgumentException
+     */
+    public function testFactoryReturnsExceptionOnNullArguments()
+    {
+        $config = array();
+
+        $client = Trakio::factory($config);
+    }
+
+    /**
+     * @expectedException \Guzzle\Common\Exception\InvalidArgumentException
+     */
+    public function testFactoryReturnsExceptionOnBlankArguments()
+    {
+        $config = array(
+            'token' => ''
+        );
+
+        $client = Trakio::factory($config);
+    }
+
+
+    public function provideConfigValues()
+    {
+        return 'iamatoken';
+    }
+}
